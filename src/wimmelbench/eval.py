82c1c1c7938a365a4526c276fb8f65bc4eab5b91
Kabir Khandpur <kabirk@live.com>
2024-11-06 11:40:25 +0000

    google ai studio model

--- src/wimmelbench/eval.py
+++ src/wimmelbench/eval.py
@@ -1,19 +1,13 @@
 import argparse
-import base64
 import os
 from typing import List
 
 from dotenv import load_dotenv
 from PIL import Image, ImageDraw
 
-from wimmelbench.models import AnthropicModel, OpenAIModel
+from wimmelbench.models import AnthropicModel, OpenAIModel, GoogleModel
 
 load_dotenv()
-
-
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
 
 
 def draw_box(image_path: str, bbox: List[float]):
@@ -26,15 +20,14 @@ def draw_box(image_path: str, bbox: List[float]):
     # Convert normalized coordinates to pixel coordinates
     box = (x1 * w, y1 * h, x2 * w, y2 * h)
 
-    # Draw rectangle
     draw.rectangle(box, outline="red", width=6)
     return image
 
 
 def get_save_path(image_path: str, object_name: str, model: str) -> str:
     filename = os.path.basename(image_path)
-    new_filename = filename.replace(".", f"_{model}_{object_name}.")
-    return os.path.join("results", new_filename)
+    new_filename = filename.replace(".", f"_{object_name}.")
+    return os.path.join("results", model.replace("/", "_"), new_filename)
 
 
 def main():
@@ -43,16 +36,34 @@ def main():
     parser.add_argument("object_name", help="Object to detect")
     args = parser.parse_args()
 
-    img = encode_image(args.image_path)
-    models = [OpenAIModel(), AnthropicModel()]
+    models = [
+        # OpenAIModel(
+        #     api_key=os.environ.get("OPENAI_API_KEY", "could-not-find-openai-api-key"),
+        #     model="gpt-4o-2024-08-06",
+        # ),
+        AnthropicModel(
+            api_key=os.environ.get(
+                "ANTHROPIC_API_KEY", "could-not-find-anthropic-api-key"
+            ),
+            model="claude-3-5-sonnet-20241022",
+        ),
+        # GoogleModel(
+        #     api_key=os.environ.get(
+        #         "GOOGLE_AISTUDIO_API_KEY", "could-not-find-google-api-key"
+        #     ),
+        #     model="gemini-1.5-pro",
+        # )
+    ]
 
     for model in models:
-        result = model.detect_object(img, args.object_name)
+        save_path = get_save_path(args.image_path, args.object_name, model.model)
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        result = model.detect_object(args.image_path, args.object_name)
         print(f"\nResult for {model.model}: {result}")
 
         if result["confidence"] > 0:
             image = draw_box(args.image_path, result["bbox"])
-            save_path = get_save_path(args.image_path, args.object_name, model.model)
             image.save(save_path, "JPEG")
             print(f"Saved image with bounding box to {save_path}")
         else:
--- src/wimmelbench/models.py
+++ src/wimmelbench/models.py
@@ -1,10 +1,20 @@
+import base64
 import json
-import os
+
+from PIL import Image
 
 from anthropic import Anthropic
 from openai import OpenAI
+import google.generativeai as genai
 
-SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it.
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+class AnthropicModel:
+    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it.
 
 Provide your result as a JSON object with the following structure:
 {
@@ -13,10 +23,7 @@ Provide your result as a JSON object with the following structure:
   "confidence": number
 }
 Where:
-- bbox: The bounding box around the object you identified.
-    - All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
-    - x1, y1: The coordinates of the top-left corner of the bounding box.
-    - x2, y2: The coordinates of the bottom-right corner of the bounding box.
+- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
 - description: A detailed description of the object you identified. Describe specifically where it is located in the image. For eg. "it is in the top third of the image, slightly left of center. It is above a red beach ball." Also describe its colour, pose, activity, and any other relevant details.
 - confidence: Your confidence score for the identification, between 0 and 1. For eg, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
 - If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
@@ -24,22 +31,18 @@ Where:
 Provide your final output ONLY in the JSON format described above.
 """
 
-
-class AnthropicModel:
-    def __init__(self, model="claude-3-5-sonnet-20241022"):
+    def __init__(self, api_key, model):
         self.client = Anthropic(
-            api_key=os.environ.get(
-                "ANTHROPIC_API_KEY", "could-not-find-anthropic-api-key"
-            ),
+            api_key=api_key,
         )
         self.model = model
 
-    def detect_object(
-        self, base64_image: str, object_name: str, max_tokens=1024
-    ) -> dict:
+    def detect_object(self, image_path: str, object_name: str, max_tokens=1024) -> dict:
+        base64_image = encode_image(image_path)
+
         response = self.client.messages.create(
             model=self.model,
-            system=SYSTEM_PROMPT,
+            system=self.SYSTEM_PROMPT,
             max_tokens=max_tokens,
             messages=[
                 {
@@ -55,7 +58,7 @@ class AnthropicModel:
                         },
                         {
                             "type": "text",
-                            "text": f"The object you should locate is: {object_name}",
+                            "text": f"Find the {object_name}",
                         },
                     ],
                 },
@@ -66,24 +69,46 @@ class AnthropicModel:
             ],
         )
 
-        data = "{" + response.content[0].text  # type: ignore
-        return json.loads(data)
+        data = json.loads("{" + response.content[0].text)  # type: ignore
+        return data
 
 
 class OpenAIModel:
-    def __init__(self, model="gpt-4o-2024-08-06"):
+    """
+    Can also be used for OpenAI-compatible APIs like OpenRouter
+    """
+
+    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it.
+
+Provide your result as a JSON object with the following structure:
+{
+  "bbox": [x1, y1, x2, y2],
+  "description": string,
+  "confidence": number
+}
+Where:
+- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
+- description: A detailed description of the object you identified. Describe specifically where it is located in the image. For eg. "it is in the top third of the image, slightly left of center. It is above a red beach ball." Also describe its colour, pose, activity, and any other relevant details.
+- confidence: Your confidence score for the identification, between 0 and 1. For eg, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
+- If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
+
+Provide your final output ONLY in the JSON format described above.
+"""
+
+    def __init__(self, api_key, model, base_url=None):
         self.client = OpenAI(
-            api_key=os.environ.get("OPENAI_API_KEY", "could-not-find-openai-api-key")
+            base_url=base_url,
+            api_key=api_key,
         )
         self.model = model
 
-    def detect_object(
-        self, base64_image: str, object_name: str, max_tokens=1024
-    ) -> dict:
+    def detect_object(self, image_path: str, object_name: str, max_tokens=1024) -> dict:
+        base64_image = encode_image(image_path)
+
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[
-                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "system", "content": self.SYSTEM_PROMPT},
                 {
                     "role": "user",
                     "content": [
@@ -95,7 +120,7 @@ class OpenAIModel:
                         },
                         {
                             "type": "text",
-                            "text": f"The object you should locate is: {object_name}",
+                            "text": f"Find the {object_name}",
                         },
                     ],
                 },
@@ -104,5 +129,55 @@ class OpenAIModel:
             response_format={"type": "json_object"},
         )
 
+        if response.error and response.error.get("code") == 429:  # type: ignore
+            raise Exception("Rate limit exceeded")
+
         data = response.choices[0].message.content
         return json.loads(data)  # type: ignore
+
+
+class GoogleModel:
+    # Gemini wants to return coordinates from 0 to 1000 in the format [ymin, xmin, ymax, xmax]
+    # https://ai.google.dev/gemini-api/docs/vision?lang=python#bbox
+    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it.
+
+Provide your result as a JSON object with the following structure:
+{
+  "bbox": [ymin, xmin, ymax, xmax],
+  "description": string,
+  "confidence": number
+}
+Where:
+- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1000, where (0,0) is the top-left corner of the image and (1000,1000) is the bottom-right corner.
+- description: A detailed description of the object you identified. Describe specifically where it is located in the image. For eg. "it is in the top third of the image, slightly left of center. It is above a red beach ball." Also describe its colour, pose, activity, and any other relevant details.
+- confidence: Your confidence score for the identification, between 0 and 1. For eg, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
+- If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
+
+Provide your final output ONLY in the JSON format described above.
+"""
+
+    def __init__(self, api_key, model):
+        genai.configure(api_key=api_key)
+        self.client = genai.GenerativeModel(
+            model, system_instruction=self.SYSTEM_PROMPT
+        )
+        self.model = model
+
+    def detect_object(self, image_path: str, object_name: str):
+        pil_image = Image.open(image_path)
+
+        response = self.client.generate_content([pil_image, "Find the {object_name}"])
+
+        # Gemini likes adding a code fence
+        data = json.loads(response.text.replace("```json\n", "").replace("\n```", ""))
+
+        # Scale bounding box coordinates to 0-1 for drawing
+        data["bbox"] = [coord / 1000 for coord in data["bbox"]]
+        # Move around x and y coordinates
+        data["bbox"] = [
+            data["bbox"][1],
+            data["bbox"][0],
+            data["bbox"][3],
+            data["bbox"][2],
+        ]
+        return data
