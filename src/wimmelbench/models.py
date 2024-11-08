2b721a27163592f1db7d229219b6332cd4683047
Kabir Khandpur <kabirk@live.com>
2024-11-08 02:20:42 +0000

    Update prompts and refactor, no confidence score

--- src/wimmelbench/models.py
+++ src/wimmelbench/models.py
@@ -7,6 +7,35 @@ from anthropic import Anthropic
 from openai import OpenAI
 import google.generativeai as genai
 
+SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked find a specific object within it. The object may not be present in the image.
+
+When asked to describe the object, return a detailed description of the object you identified. Describe specifically where it is located in the image. Also describe its colour, pose, activity, and any other relevant details.
+
+Provide your result as a JSON object with the following structure:
+{json_prompt}
+"""
+
+DEFAULT_JSON_PROMPT = """
+{
+  "bbox": [x1, y1, x2, y2],
+  "description": string
+}
+
+- bbox: The bounding box around the object you identified.
+- All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
+- If you do not see the object, provide bounding box coordinates of [0, 0, 0, 0]."""
+
+# Gemini wants to return coordinates from 0 to 1000 in the format [ymin, xmin, ymax, xmax]
+# https://ai.google.dev/gemini-api/docs/vision?lang=python#bbox
+GEMINI_JSON_PROMPT = """
+{
+  "bbox": [ymin, xmin, ymax, xmax],
+  "description": string
+}
+
+- bbox: The bounding box around the object you identified.
+- If you do not see the object, provide bounding box coordinates of [0, 0, 0, 0]."""
+
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
@@ -14,25 +43,6 @@ def encode_image(image_path):
 
 
 class AnthropicModel:
-    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it. The object may not be present in the image.
-
-Provide your result as a JSON object with the following structure:
-{
-  "bbox": [x1, y1, x2, y2],
-  "description": string,
-  "confidence": number
-}
-Where:
-- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
-- description: A detailed description of the object you identified. Describe specifically where it is located in the image. Also describe its colour, pose, activity, and any other relevant details.
-  - An example description: "The red beach ball is near the top of the image, slightly left of center. It is above an umbrella and between two palm trees. The ball looks brand new."
-- confidence: Your confidence score for the identification, between 0 and 1. For example, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
-
-If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
-
-Provide your final output ONLY in the JSON format described above.
-"""
-
     def __init__(self, api_key, model):
         self.client = Anthropic(
             api_key=api_key,
@@ -44,7 +54,7 @@ Provide your final output ONLY in the JSON format described above.
 
         response = self.client.messages.create(
             model=self.model,
-            system=self.SYSTEM_PROMPT,
+            system=SYSTEM_PROMPT.format(json_prompt=DEFAULT_JSON_PROMPT),
             max_tokens=max_tokens,
             messages=[
                 {
@@ -64,40 +74,16 @@ Provide your final output ONLY in the JSON format described above.
                         },
                     ],
                 },
-                {
-                    "role": "assistant",
-                    "content": "{",
-                },
             ],
         )
 
-        data = json.loads("{" + response.content[0].text)  # type: ignore
-        return data
+        return json.loads(response.content[0].text)  # type: ignore
 
 
 class OpenAIModel:
     """
     Can also be used for OpenAI-compatible APIs like OpenRouter
     """
-
-    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it. The object may not be present in the image.
-
-Provide your result as a JSON object with the following structure:
-{
-  "bbox": [x1, y1, x2, y2],
-  "description": string,
-  "confidence": number
-}
-Where:
-- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1, where (0,0) is the top-left corner of the image and (1,1) is the bottom-right corner.
-- description: A detailed description of the object you identified. Describe specifically where it is located in the image. Also describe its colour, pose, activity, and any other relevant details.
-  - An example description: "The red beach ball is near the top of the image, slightly left of center. It is above an umbrella and between two palm trees. The ball looks brand new."
-- confidence: Your confidence score for the identification, between 0 and 1. For example, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
-
-If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
-
-Provide your final output ONLY in the JSON format described above.
-"""
 
     def __init__(self, api_key, model, base_url=None):
         self.client = OpenAI(
@@ -112,7 +98,10 @@ Provide your final output ONLY in the JSON format described above.
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[
-                {"role": "system", "content": self.SYSTEM_PROMPT},
+                {
+                    "role": "system",
+                    "content": SYSTEM_PROMPT.format(json_prompt=DEFAULT_JSON_PROMPT),
+                },
                 {
                     "role": "user",
                     "content": [
@@ -146,31 +135,11 @@ Provide your final output ONLY in the JSON format described above.
 
 
 class GoogleModel:
-    # Gemini wants to return coordinates from 0 to 1000 in the format [ymin, xmin, ymax, xmax]
-    # https://ai.google.dev/gemini-api/docs/vision?lang=python#bbox
-    SYSTEM_PROMPT = """You are an expert computer vision system. You will be given an image and asked to find a specific object within it. The object may not be present in the image.
-
-Provide your result as a JSON object with the following structure:
-{
-  "bbox": [ymin, xmin, ymax, xmax],
-  "description": string,
-  "confidence": number
-}
-Where:
-- bbox: The bounding box around the object you identified. All coordinates should be normalized to be between 0 and 1000, where (0,0) is the top-left corner of the image and (1000,1000) is the bottom-right corner.
-- description: A detailed description of the object you identified. Describe specifically where it is located in the image. Also describe its colour, pose, activity, and any other relevant details.
-  - An example description: "The red beach ball is near the top of the image, slightly left of center. It is above an umbrella and between two palm trees. The ball looks brand new."
-- confidence: Your confidence score for the identification, between 0 and 1. For example, if you are 50% sure the object is where you have described, provide a confidence of 0.5.
-
-If you do not see the object, provide a confidence score of 0 and the bounding box coordinates as [0, 0, 0, 0].
-
-Provide your final output ONLY in the JSON format described above.
-"""
-
     def __init__(self, api_key, model):
         genai.configure(api_key=api_key)
         self.client = genai.GenerativeModel(
-            model, system_instruction=self.SYSTEM_PROMPT
+            model,
+            system_instruction=SYSTEM_PROMPT.format(json_prompt=GEMINI_JSON_PROMPT),
         )
         self.model = model
 
@@ -179,16 +148,18 @@ Provide your final output ONLY in the JSON format described above.
 
         response = self.client.generate_content([pil_image, f"Find the {object_name}"])
 
-        # Gemini likes adding a code fence
+        # There's usually a code fence
         data = json.loads(response.text.replace("```json\n", "").replace("\n```", ""))
+        bbox = data["bbox"]
 
-        # Scale bounding box coordinates to 0-1 for drawing
-        data["bbox"] = [coord / 1000 for coord in data["bbox"]]
-        # Move around x and y coordinates
-        data["bbox"] = [
-            data["bbox"][1],
-            data["bbox"][0],
-            data["bbox"][3],
-            data["bbox"][2],
+        bbox = [
+            bbox[1] / 1000,  # xmin
+            bbox[0] / 1000,  # ymin
+            bbox[3] / 1000,  # xmax
+            bbox[2] / 1000,  # ymax
         ]
-        return data
+
+        return {
+            "description": data["description"],
+            "bbox": bbox,
+        }
